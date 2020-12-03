bf8b1e79ea308080007315ca90857673faf302ee
igmrrf <francis.igbiriki.fi@gmail.com>
2020-12-03 22:53:41 +0100

    initial Blog

---
+++ src/pages/page-3.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ThirdPage = () => (
+  <Layout>
+    <SEO title="Page Three" />
+    <h1>Hi from the Third page</h1>
+  </Layout>
+)
+
+export default ThirdPage
